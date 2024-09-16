c886cf28369944e341e69fd2b406e1858ea097b8
Pep_$xx <pep_sxx@mail.ru>
2024-09-16 13:42:47 +0300

    Add YML

---
+++ src/main/java/com/code/pep/pep_aston_cln/PepAstonClnApplication.java
@@ -0,0 +1,14 @@
+package com.code.pep.pep_aston_cln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepAstonClnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PepAstonClnApplication.class, args);
+        System.out.println("PepAstonClnApplication started");
+    }
+
+}
---
+++ src/test/java/com/code/pep/pep_aston_cln/PepAstonClnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.pep.pep_aston_cln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepAstonClnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
