09d2f36a59c62f1ab7babaec796659ca84783d94
Pep_$xx <pep_sxx@mail.ru>
2024-09-16 20:43:41 +0300

    Betta

--- src/main/java/com/code/pep/pep_aston_cln/PepAstonClnApplication.java
+++ src/main/java/com/code/pep/pep_aston_cln/PepAstonClnApplication.java
@@ -9,7 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class PepAstonClnApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PepAstonClnApplication.class, args);
+
     }
 
 }
